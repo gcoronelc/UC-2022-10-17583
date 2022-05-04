351bd04e14aff18979b07ac75b19eb23513e3cca
Gustavo Coronel <gcoronelc@gmail.com>
2022-05-04 11:31:59 -0500

    Semana 7

---
+++ Semana07/CoronelCastilloGustavo.cpp
@@ -0,0 +1,65 @@
+// Estudiante: Eric Gustavo Coronel Castillo
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Variables
+	int opMenu;
+	
+	// Control del menú de opciones
+	do{
+		// EL MENU
+		system("cls");
+		cout << "SOBRE RUEDAS SAC" << endl;
+		cout << "Manú de opciones" << endl;
+		cout << "=================================================" << endl;
+		cout << "1.- Procesar venta" << endl;
+		cout << "2.- Listado de ventas1" << endl;
+		cout << "3.- Resumen de ventas" << endl;
+		cout << "4.- Salir" << endl;
+		cout << "=================================================" << endl;
+		// Leer opción
+		cout << endl;
+		do{
+			cout << "Opcion: "; cin >> opMenu;
+			if(opMenu<1 || opMenu>4){
+				cout << "ERROR: Opción incorrecta, intentalo nuevamente." << endl;
+			}
+		}while(opMenu<1 || opMenu>4);
+		
+		// Procesar opción del menú
+		system("cls");
+		switch(opMenu){
+			// Opción 1: aaaa
+			case 1:
+				cout << "Proceso opción 1" << endl;
+				break;
+			// Opción 2: bbbbb
+			case 2:
+				cout << "Proceso opción 2" << endl;
+				break;	
+			// Opción 3: cccccccc
+			case 3:
+				cout << "Proceso opción 3" << endl;
+				break;					
+			// Opción 4: dddddddd
+			case 4:
+				cout << "Proceso opción 4" << endl;
+				break;
+			// Opción 5: Salir
+			case 5:
+				cout << "Fin del programa." << endl;
+				break;
+		}
+		
+		system("pause");
+	} while(opMenu<4);
+	
+	// Fin
+	return 0;
+}
