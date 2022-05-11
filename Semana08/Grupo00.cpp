5b851ce112f52a8e32ffef5f7f423dcca2a32a90
Gustavo Coronel <gcoronelc@gmail.com>
2022-05-11 12:32:55 -0500

    Semana 8

--- Semana08/Grupo00.cpp
+++ Semana08/Grupo00.cpp
@@ -19,7 +19,14 @@ int main(){
 	setlocale(LC_CTYPE,"Spanish");
 	
 	// Variables
-	int opMenu;
+	int opMenu, tipoPlato, cantidad, edad;
+	double precio, porcDescuento1, porcDescuento2;
+	char sexo;
+	double importeBruto, descuento1, descuento2, importeNeto;
+	
+	// Variables de indicadores
+	int cantTotalVentas=0, cantVentasMujeresDcto2=0, cantVentasHombresDcto1=0;
+	double sumaTotalVentasBruto=0, sumaTotalVentasNeto=0, sumaTotalDescuentos=0; 	
 	
 	// Control del menú de opciones
 	do{
@@ -45,24 +52,122 @@ int main(){
 		// Procesar opción del menú
 		system("cls");
 		switch(opMenu){
-			// Opción 1: aaaa
+			// Opción 1: Venta
 			case 1:
-				cout << "Proceso opción 1" << endl;
+				// Cabecera
+				cout << "LA CASA DEL INKA" << endl;
+				cout << "SISTEMA DE VENTAS" << endl << endl;
+				cout << "LECTURA DE DATOS" << endl;
+				cout << "============================================" << endl;
+				// Leer el tipo de plato
+				cout << "TIPO DE PLATO" << endl;
+				cout << "\t1.- Carnes" << endl;
+				cout << "\t2.- Pescados" << endl;
+				cout << "\t3.- Vinos" << endl;
+				do{
+					cout << "Opción: "; cin >> tipoPlato;
+					if(tipoPlato<1 || tipoPlato>3){
+						cout << "ERROR: tipo de plato incorrecto." << endl;
+					}
+				}while(tipoPlato<1 || tipoPlato>3);
+				// Leer precio
+				do{
+					cout << "Precio: "; cin >> precio;
+					if(precio<=0.0){
+						cout << "ERROR: el precio es un valor positivo." << endl;
+					}
+				}while(precio<=0.0);
+				// Leer cantidad
+				do{
+					cout << "Cantidad: "; cin >> cantidad;
+					if(cantidad<=0){
+						cout << "ERROR: la cantidad es un valor positivo." << endl;
+					}
+				}while(cantidad<=0);
+				// Leer sexo
+				do{
+					cout << "Sexo (M/F): "; cin >> sexo;
+					sexo = toupper(sexo);
+					if(sexo!='M' && sexo!='F'){
+						cout << "ERROR: ingrese M o F." << endl;
+					}
+				}while(sexo!='M' && sexo!='F');
+				// Leer edad
+				do{
+					cout << "Edad: "; cin >> edad;
+					if(edad<=0 || edad>120){
+						cout << "ERROR: ingrese un valor correcto." << endl;
+					}
+				}while(edad<=0 || edad>120);
+				// PROCESO
+				// Encontrar el % de descuento 1
+				switch(tipoPlato){
+					case 1:
+						porcDescuento1 = 0.0;
+						porcDescuento1 = (cantidad>=5 && cantidad<=10)?5:porcDescuento1;
+						porcDescuento1 = (cantidad>10)?8:porcDescuento1;
+						break;
+					case 2:
+						porcDescuento1 = 0.0;
+						porcDescuento1 = (cantidad>=5 && cantidad<=10)?6:porcDescuento1;
+						porcDescuento1 = (cantidad>10)?10:porcDescuento1;
+						break;
+					case 3:
+						porcDescuento1 = 0.0;
+						porcDescuento1 = (cantidad>=5 && cantidad<=10)?8:porcDescuento1;
+						porcDescuento1 = (cantidad>10)?14:porcDescuento1;
+						break;												
+				}
+				// Encontrar el % de descuento 1
+				switch(sexo){
+					case 'M':
+						porcDescuento2 = 0.0;
+						porcDescuento2 = (edad>=30 && edad<=40)?6:porcDescuento2;
+						porcDescuento2 = (edad>40)?8:porcDescuento2;
+						break;
+					case 'F':
+						porcDescuento2 = 0.0;
+						porcDescuento2 = (edad>=30 && edad<=40)?8:porcDescuento2;
+						porcDescuento2 = (edad>40)?10:porcDescuento2;
+						break;
+				}
+				// Calculo de la venta
+				importeBruto = precio * cantidad;
+				descuento1 = importeBruto * porcDescuento1 / 100.0;
+				descuento2 = importeBruto * porcDescuento2 / 100.0;
+				importeNeto = importeBruto - descuento1 - descuento2;
+				// REPORTE
+				cout << endl;
+				cout << "REPORTE" << endl;
+				cout << "==============================================" << endl;
+				cout << "Importe bruto: " << importeBruto << endl;
+				cout << "Descuento 1: " << descuento1 << endl;
+				cout << "Descuento 2: " << descuento2 << endl;
+				cout << "Importe neto: " << importeNeto << endl;
+				cout << endl;
+				// Actualizar indicadores
+				cantTotalVentas++;
+				cantVentasMujeresDcto2 += (sexo=='F' && descuento2>0)?1:0;
+				cantVentasHombresDcto1 += (sexo=='M' && descuento1>0)?1:0;
+				sumaTotalVentasBruto += importeBruto;
+				sumaTotalVentasNeto += importeNeto;
+				sumaTotalDescuentos += (descuento1 + descuento2);
 				break;
-			// Opción 2: bbbbb
+				
+			// Opción 2: Reporte
 			case 2:
-				cout << "Proceso opción 2" << endl;
+				cout << "REPORTE DE INDICADORES" << endl;
+				cout << "=======================================================================" << endl;
+				cout << "Cantidad total de ventas: " << cantTotalVentas << endl;
+				cout << "Cantidad de ventas a clientes femeninos con el segundo descuento: " << cantVentasMujeresDcto2 << endl;
+				cout << "Cantidad de ventas a clientes masculinos solo con el primer descuento: " << cantVentasHombresDcto1 << endl;
+				cout << "Suma total de las ventas (Importe Bruto): " << sumaTotalVentasBruto << endl;
+				cout << "Suma total de las ventas (Importe Neto): " << sumaTotalVentasNeto << endl;
+				cout << "Suma de todos los descuentos realizados: " << sumaTotalDescuentos << endl;
+				cout << endl;
 				break;	
-			// Opción 3: cccccccc
+			// Opción 3: Salir
 			case 3:
-				cout << "Proceso opción 3" << endl;
-				break;					
-			// Opción 4: dddddddd
-			case 4:
-				cout << "Proceso opción 4" << endl;
-				break;
-			// Opción 5: Salir
-			case 5:
 				cout << "Fin del programa." << endl;
 				break;
 		}
