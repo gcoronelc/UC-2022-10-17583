96700cbf7816bd5e1efd7cc7466359aabeac5bed
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-01 12:47:18 -0500

    Semana 11

--- Semana11/Ejercicio05.cpp
+++ Semana11/Ejercicio05.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 // Variables globales
+int cantPuntaCana=0, cantSanAndres=0, cantCancun=0;
+double totalPuntaCana=0.0, totalSanAndres=0.0, totalCancun=0.0;
+double totalAcumulado=0.0;
 
 // Prototipo de funciones y procedimientos
 void mostrarMenu();
@@ -78,6 +81,8 @@ int leerOpcion(int opMin, int opMax){
 void procesarVenta(){
 	// Vriables
 	int paquete, cantidad;
+	double precio, porcDescuento;
+	double importe, descuento, total;
 	// Lectura
 	cout << "PAQUETE TURISTICO" << endl;
 	cout << "================================" << endl;
@@ -88,14 +93,55 @@ void procesarVenta(){
 	paquete = leerOpcion(1,3);
 	cantidad = LeerEntero("Cantidad: ",1);
 	// Proceso
-	
-	
+	// 1.- Determinar el precio y el porcentaje
+	switch(paquete){
+		case 1:
+			precio = 780.0;
+			porcDescuento = 3.5;
+			break;
+		case 2:
+			precio = 1350.0;
+			porcDescuento = 4.0;
+			break;
+		case 3:
+			precio = 2550.0;
+			porcDescuento = 4.5;
+			break;
+	}
+	// 2.- Calculos de la venta
+	importe = precio * cantidad;
+	descuento = importe * porcDescuento / 100.0;
+	total = importe - descuento;
+	// 3.- Estadistica
+	cantPuntaCana += (paquete==1)?cantidad:0;
+	cantSanAndres += (paquete==2)?cantidad:0;
+	cantCancun += (paquete==3)?cantidad:0;
+	totalPuntaCana += (paquete==1)?total:0;
+	totalSanAndres += (paquete==2)?total:0;
+	totalCancun += (paquete==3)?total:0;
+	totalAcumulado += total;
 	// Reporte
-	
+	cout << endl;
+	cout << "REPORTE" << endl;
+	cout << "================================" << endl;
+	cout << "Importe: " << importe << endl;
+	cout << "Porc. descuento: " << porcDescuento << "%" << endl;
+	cout << "Descuento: " << descuento << endl;
+	cout << "Total: " << total << endl;
+	cout << endl;
 }
 void reporteVentas(){
-	cout << "Algo";
-	
+	cout << endl;
+	cout << "REPORTE" << endl;
+	cout << "==============================================================" << endl;
+	cout << "PQUETE\t\tCANTIDAD\tIMPORTE" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "Punta Cana\t\t" << cantPuntaCana << "\t\t" << totalPuntaCana << endl;
+	cout << "San Andrea\t\t" << cantSanAndres << "\t\t" << totalSanAndres << endl;
+	cout << "Cancun\t\t\t" << cantCancun << "\t\t" << totalCancun << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "Total acumulado: " << totalAcumulado << endl;
+	cout << endl;
 }
 void salir(){
 	cout << "PROCESO SALIR" << endl;
