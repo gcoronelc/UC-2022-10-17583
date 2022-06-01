f6d51132aaf7442123a5e0495d8d5ab03c820eb1
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-01 11:58:44 -0500

    Semana 11

--- Semana11/Ejercicio05.cpp
+++ Semana11/Ejercicio05.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 // Prototipo de funciones y procedimientos
 void mostrarMenu();
-
+int leerOpcion(int opMin, int opMax);
+void procesarVenta();
+void reporteVentas();
+void salir();
 
 int main(){
 	
@@ -15,11 +18,24 @@ int main(){
 	int opMenu;
 	// Proceso general
 	do{
+		// El menú
 		mostrarMenu();
-		
-		
+		opMenu = leerOpcion(1,3);
+		// Proceso general
+		system("cls");
+		switch(opMenu){
+			case 1:
+				procesarVenta();
+				break;
+			case 2:
+				reporteVentas();
+				break;
+			case 3:
+				salir();
+				break;
+		}		// Fin
 		system("pause");
-	} while(false);
+	} while(opMenu!=3);
 	
 	// Fin
 	return 0;
@@ -35,3 +51,38 @@ void mostrarMenu(){
 	cout << "\t3.- Salir" << endl;
 	cout << "=====================================" << endl;
 }
+
+int leerOpcion(int opMin, int opMax){
+	int op;
+	do{
+		cout << "Opción: "; cin >> op;
+		if(op<opMin || op>opMax){
+			cout << "Error, opción fuera de rango." << endl;
+		}
+	} while(op<opMin || op>opMax);
+	return op;
+}
+
+void procesarVenta(){
+	
+}
+void reporteVentas(){
+	cout << "Algo";
+	
+}
+void salir(){
+	cout << "PROCESO SALIR" << endl;
+	cout << "================================================" << endl;
+	cout << "Fin del programa" << endl;
+	cout << "Nos vemos en el mundial" << endl;
+}
+
+
+
+
+
+
+
+
+
+
