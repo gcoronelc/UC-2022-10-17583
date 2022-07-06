0d8403a923c035b46446244719e1969bbb2ef51f
Gustavo Coronel <gcoronelc@gmail.com>
2022-07-06 11:09:45 -0500

    Semana 16

---
+++ Semana16/Grupo20/Programa20.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Variables
+	
+	
+	// Lectura de datos
+	
+	
+	// Proceso
+	
+	
+	// Reporte
+	
+	
+	// Fin
+	system("pause");
+	return 0;
+}
